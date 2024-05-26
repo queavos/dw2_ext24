29a1793d31615b2668d3ef074920db31355d6ab7
Osvaldo Micniuk <queavos.dev@gmail.com>
2024-05-26 16:00:09 -0400

    index funciona

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
-
-
-<?php include '../includes/navbar.php'; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestión de Materias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<?php include 'includes/navbar.php'; ?>
 <div class="container mt-5">
     
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
 
 
 
